d3d89ab65b706551e3fc647da8d3d9f46e00fdf7
MZachura <34230524+MZachura@users.noreply.github.com>
2020-11-10 18:00:53 +0100

    podpięcie menu do gry. Do poprawy ale działa +-

--- classes/gui/menu.py
+++ classes/gui/menu.py
@@ -1,33 +1,41 @@
-from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication)
-#from classes.game.game import Game
+from PyQt5.QtWidgets import (QWidget,QMainWindow, QPushButton, QVBoxLayout, QApplication)
+from classes.game.game import Game
+import sys
 
-class Menu(QWidget):
+class Menu(QMainWindow):
 
     def __init__(self):
-        super().__init__()
-
+        super(Menu,self).__init__()
+        
+        self.setGeometry(200,200,300,300)
+        self.setWindowTitle("Zombie Rush")
         self.initUI()
 
     def initUI(self):
+        self.playbutton = QPushButton(self)
+        self.playbutton.setText("PLAY")
+        self.playbutton.move(100,50)
+        self.playbutton.clicked.connect(self.playbuttonclicked)
+        self.scorebutton = QPushButton(self)
+        self.scorebutton.setText("SCORE")
+        self.scorebutton.clicked.connect(self.scorebuttonclicked)
+        self.scorebutton.move(100,100)        
+        self.exitbutton = QPushButton(self)
+        self.exitbutton.setText("EXIT")
+        self.exitbutton.clicked.connect(self.exitbuttonclicked)
+        self.exitbutton.move(100,200) 
+        
+    def playbuttonclicked(self):
+        g= Game()
+        g.set_up()
+        g.mainloop()
+        sys.exit(app.exec_())
+    def exitbuttonclicked(self):
+        sys.exit(app.exec_())
+    def scorebuttonclicked(self):
+        print("score")
 
-        playButton = QPushButton("Play")
-        loadButton = QPushButton("Load Game")
-        exitButton = QPushButton("Exit")
-        scoreButton = QPushButton("High Scores")
 
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(playButton)
-        hbox.addWidget(loadButton)
-        hbox.addWidget(scoreButton)
-        hbox.addWidget(exitButton)
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-
-        self.setLayout(vbox)
-
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Hotline Clone')
-        self.show()
+        
+# geometry 300 300 300 300 ; title zombie rush, buttons: play, load,exit, highscores; layout qvbox
+  
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import sys
-from classes.game.game import Game
 from classes.gui.menu import Menu
 from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication)
 
 def main():
-    g = Game()
-    g.set_up()
-    g.mainloop()
-    #app = QApplication(sys.argv)
-    #m = Menu()
-    #sys.exit(app.exec_())
-
-
+    app = QApplication(sys.argv)
+    menu = Menu()
+    menu.show()
+    sys.exit(app.exec_())
 if __name__ == "__main__":
     main()
               
